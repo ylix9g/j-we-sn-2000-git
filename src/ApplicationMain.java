8d8f8c5685aac49ae7127e4c69bcaf18a1125a15
Igor Polichshuk <ylix9g@gmail.com>
2024-04-03 21:11:31 +0500

    Added demostration for insertion sort algo

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class ApplicationMain {
 
     public static void main(String[] args) {
-        System.out.println("Java Application");
+        System.out.println("Java Sort Application");
+
+        int[] array = new int[]{5, -8, 9, 15};
+        Sort sort = new InsertionSort();
+        sort.sort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
