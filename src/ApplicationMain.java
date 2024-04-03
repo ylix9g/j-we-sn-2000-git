5fc0e7b4868bd7246250fdc09587a6cffad9801e
Igor Polichshuk <ylix9g@gmail.com>
2024-04-03 21:09:42 +0500

    Repeat system ouput in main() 3 times

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -1,6 +1,8 @@
 public class ApplicationMain {
 
     public static void main(String[] args) {
-        System.out.println("Java Application");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Java Application");
+        }
     }
 }
